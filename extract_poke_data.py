be4e094e34bee5246cf6d2b6871352fa21235ce6
tau <36781951+tndd@users.noreply.github.com>
2024-01-07 20:49:24 +0900

    impl extract_dominant_pokemon

--- extract_poke_data.py
+++ extract_poke_data.py
@@ -2,13 +2,6 @@ import json
 
 from bs4 import BeautifulSoup
 
-# HTMLファイルを読み込む
-with open('mock/pokemon.html', 'r', encoding='utf-8') as file:
-    contents = file.read()
-
-# BeautifulSoupオブジェクトを作成
-soup = BeautifulSoup(contents, 'html.parser')
-
 
 def extract_same_team(soup):
     # "同じチーム"というテキストを持つspanを見つける
@@ -29,3 +22,38 @@ def extract_same_team(soup):
             # 順位とポケモン名を辞書に追加
             same_team[rank] = pokemon_name
     return same_team
+
+
+def extract_dominant_pokemon(soup):
+    # "同じチーム"というテキストを持つspanを見つける
+    dominant_pokemon = soup.find('p', string="倒したポケモン")
+    # spanから最も近い親要素でクラスが'pokemon-trend-card'のdivを見つける
+    div_pokemon_trend_card = dominant_pokemon.find_parent(class_="pokemon-trend-card")
+    # 同じチーム数の順位と名前のペア
+    dominant_pokemon = {}
+    # ペア情報の取得
+    for tr in div_pokemon_trend_card.find_all('tr'):
+        # 各行のセルを取得
+        cells = tr.find_all('td')
+        if cells and len(cells) >= 2:
+            # 順位を取得
+            rank = cells[0].get_text(strip=True)
+            # ポケモン名を取得
+            pokemon_name = cells[1].find('a', class_="pokemon-name").get_text(strip=True)
+            # 順位とポケモン名を辞書に追加
+            dominant_pokemon[rank] = pokemon_name
+    return dominant_pokemon
+
+
+def extract_disadvantage_pokemon(soup):
+    pass
+
+
+if __name__ == '__main__':
+    # HTMLファイルを読み込む
+    with open('mock/pokemon.html', 'r', encoding='utf-8') as file:
+        contents = file.read()
+    # BeautifulSoupオブジェクトを作成
+    soup = BeautifulSoup(contents, 'html.parser')
+    d = extract_dominant_pokemon(soup)
+    print(d)
